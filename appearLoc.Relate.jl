eddc2130918c6b7f21253042e571421b4ed66976
佐藤陽花 <csta19097@g.nihon-u.ac.jp>
2021-08-02 02:36:19 +0900

    Update appearLoc.Relate.jl

--- appearLoc.Relate.jl
+++ appearLoc.Relate.jl
@@ -1,1 +1,15 @@
 
+function relaIO(fileNameTw,fileNameLuuk,fileNameOut)
+  fTw = open(fileNameTw,"r")
+  dataTw = readLines(fTw)
+  close(fTw)
+  fLu = open(fileNameLuuk,"r")
+  luuks = readLines(fLu)
+  close(fLu)
+  outData = linker(dataTw,luuks)
+  fOut = open(fileNameOut,"w")
+  write(fOut,outData)
+  close(fOut)
+end
+function linker(tweets,luuks)
+end
